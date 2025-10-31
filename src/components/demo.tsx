a5c63b418ffd02e79768700ad4512fd77945c2bb
Camresh James <njengajamesmwangi@gmail.com>
2025-10-31 08:46:31 +0300

    hosting

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,14 +1,51 @@
+import { useState } from 'react';
 import { FormEngine } from '../engine/FormEngine';
-import { insuranceQuoteSchema as schema} from '../../formSchemas';
+import {
+  insuranceQuoteSchema,
+  contactFormSchema,
+  surveyFormSchema,
+  registrationFormSchema,
+  agentUpdateSchema,
+  productFormSchema,
+  addressFormSchema,
+  jobApplicationSchema,
+} from '../../formSchemas';
+
+const schemas = {
+  insuranceQuote: insuranceQuoteSchema,
+  contactForm: contactFormSchema,
+  surveyForm: surveyFormSchema,
+  registrationForm: registrationFormSchema,
+  agentUpdate: agentUpdateSchema,
+  productForm: productFormSchema,
+  addressForm: addressFormSchema,
+  jobApplication: jobApplicationSchema,
+};
 
 export const Demo = () => {
-    return (
-        <FormEngine
-            schema={schema}
-            onSubmit={values => {
-                console.log('Submitted values:', values);
-                alert('Check console - form submitted!');
-            }}
-        />
-    );
+  const [selectedSchema, setSelectedSchema] = useState<keyof typeof schemas>('insuranceQuote');
+
+  return (
+    <div className="p-4 space-y-4">
+      <select
+        value={selectedSchema}
+        onChange={e => setSelectedSchema(e.target.value as keyof typeof schemas)}
+        className="border rounded p-2"
+      >
+        {Object.keys(schemas).map(key => (
+          <option key={key} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+
+      <FormEngine
+        schema={schemas[selectedSchema]}
+        onSubmit={values => {
+          console.log('Submitted values:', values);
+          alert('Check console - form submitted!');
+        }}
+      />
+    </div>
+  );
 };
